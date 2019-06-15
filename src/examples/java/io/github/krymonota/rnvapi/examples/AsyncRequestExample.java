15bc6cea94667a918a87e4ffcf737497d1676ff2
Niklas (Krymonota) <Krymonota@users.noreply.github.com>
2019-06-15 17:45:16 +0200

    Update code examples

--- src/examples/java/io/github/krymonota/rnvapi/examples/AsyncRequestExample.java
+++ src/examples/java/io/github/krymonota/rnvapi/examples/AsyncRequestExample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Krymonota
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.krymonota.rnvapi.examples;
 
 import io.github.krymonota.rnvapi.RNVAPI;
@@ -14,7 +30,7 @@ public class AsyncRequestExample {
 
     public static void main(String[] args) {
         final RNVAPIService apiService = RNVAPI.createAPIServiceProvider(args[0]);
-        
+
         // Asynchronous Request
         apiService.getStationInfo("1", null, null).enqueue(new Callback<List<StationInfoTransfer>>() {
 
@@ -30,10 +46,10 @@ public class AsyncRequestExample {
 
             @Override
             public void onFailure(Call<List<StationInfoTransfer>> call, Throwable t) {
-                // Something went completely wrong (e. g. no internet connection)
+                // Something went terribly wrong (e. g. no internet connection)
                 t.printStackTrace();
             }
-            
+
         });
     }
 
--- src/examples/java/io/github/krymonota/rnvapi/examples/SyncRequestExample.java
+++ src/examples/java/io/github/krymonota/rnvapi/examples/SyncRequestExample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Krymonota
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.krymonota.rnvapi.examples;
 
 import java.io.IOException;
@@ -11,11 +27,11 @@ public class SyncRequestExample {
 
     public static void main(String[] args) {
         final RNVAPIService apiService = RNVAPI.createAPIServiceProvider(args[0]);
-        
+
         try {
             // Synchronous Request
-            String result = new Gson().toJson(apiService.getAllLines().execute().body());
-            
+            final String result = new Gson().toJson(apiService.getAllLines().execute().body());
+
             System.out.println(result);
         } catch (IOException e) {
          // Something went wrong
